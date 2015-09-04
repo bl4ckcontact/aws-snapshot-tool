3d34acd6b044cf6fd0a7bee025a22deb03d295c1
Alex Black <bl4ckcontact@gmail.com>
2015-09-04 00:21:58 -0700

    check for volumes with key/value 'Snapshot' = 'true'

--- aws-snapshots.py
+++ aws-snapshots.py
@@ -30,23 +30,25 @@ def main():
         if v.attach_data.instance_id is not None:
 
             instance_name = bh.get_instance_name(v.attach_data.instance_id)
-            vol_name = v.__dict__['tags']['Name']
+            vol_name = v.tags['Name']
             snapshot_prefix = str(datetime.date.today()) + "_"
             snapshot_description = snapshot_prefix.replace(" ", "_") + vol_name.replace(" ", "_")
 
-            try:
-                logger.info("Attempting to snapshot '%s' on instance '%s'" % (vol_name, instance_name))
-                backup_result = bh.backup_instance(instance_name, snapshot_prefix)
-            except Exception, ex:
-                if ex.status == 403:
-                    backup_result = ex.reason.upper() + ": " + "Access denied."
-                    logger.error(backup_result)
-                else:
-                    backup_result = 'ERROR: ' + ex.__dict__ + ex.error_message
-            finally:
-                backup_result = str(backup_result)
-
-            pt.add_row([instance_name, vol_name, snapshot_description, backup_result])
+            if v.tags.get('Snapshot') == 'true':
+                try:
+                    logger.info("Attempting to snapshot '%s' on instance '%s'" % (vol_name, instance_name))
+                    backup_result = bh.backup_instance(instance_name, snapshot_prefix)
+                except Exception, ex:
+                    if ex.status == 403:
+                        backup_result = ex.reason.upper() + ": " + "Access denied."
+                        logger.error(backup_result)
+                    else:
+                        backup_result = 'ERROR: ' + ex.error_message
+                finally:
+                    backup_result = str(backup_result)
+                pt.add_row([instance_name, vol_name, snapshot_description, backup_result])
+            else:
+                pass
 
     print pt
 
