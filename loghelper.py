17d250879cb9ff7f7b8a0de923072f69fb73bc74
Alex Black <bl4ckcontact@gmail.com>
2015-09-13 23:27:25 -0700

    formatting change to loghelper

--- loghelper.py
+++ loghelper.py
@@ -5,7 +5,6 @@ import logging
 def logHelper(fileName, logLevel=logging.INFO, useConsole=True):
     """
     Simple Logging Helper. Retuens logger reference.
-
     Paramsmeters:
     fileName: Filename, may include full path, or will open a file in default folder
     logLevel: Pass logging.INFO, logging.DEBUG or other enums for logging level
@@ -15,7 +14,8 @@ def logHelper(fileName, logLevel=logging.INFO, useConsole=True):
     # init logging
     log = logging.getLogger()
     log.setLevel(logLevel)
-    logFormatter = logging.Formatter("%(asctime)s | %(threadName)-12.12s | %(levelname)-5.5s | %(message)s")
+    logFormatter = logging.Formatter(fmt='%(asctime)s [%(levelname)8s] %(message)s',
+                                     datefmt="%y-%m-%d %h:%m:%s")
 
     # file handler
     fileOut = logging.FileHandler(fileName)
